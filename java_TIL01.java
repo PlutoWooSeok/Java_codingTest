cc41648826e69a0f3024777818a71c8dab8cc48e
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-08-09 15:25:54 +0900

    variable decl &save

--- java_TIL01.java
+++ java_TIL01.java
@@ -12,10 +12,38 @@ public class java_TIL01 {
 
         //계산값 출력
         //예제2-3
-        System.out.println("5+3");
-        System.out.println("5-3");
-        System.out.println("5*3");
-        System.out.println("5/3");
+//        System.out.println("5+3");
+//        System.out.println("5-3");
+//        System.out.println("5*3");
+//        System.out.println("5/3");
+        //변수의 선언과 저장
+        //예제 2-4
+        int x=5;
+        System.out.println(x);
+
+        x=10;
+        System.out.println(x);
+
+        //예제 2-5
+        int x1=10;
+        int y=5;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+        //변수의 타입
+//        int x=100;
+//        double pi = 3.14;
+//        char ch='a';
+//        String str ="abc";
+//
+//        System.out.println(x);
+//        System.out.println(pi);
+//        System.out.println(ch);
+//        System.out.println(str);
+
 
     }
 
