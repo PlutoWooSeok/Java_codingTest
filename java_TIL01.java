2af3b1f876d341ff3f2ae71e262bc0ff2535c001
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-08-09 15:21:35 +0900

    cal print

--- java_TIL01.java
+++ java_TIL01.java
@@ -2,13 +2,21 @@ public class java_TIL01 {
     public static void main(String[] args){
         //화면에 글자 출력하기
         // 예제 2-1
-        System.out.println("hello world");
-        System.out.print("hello");
-        System.out.println("world");
+//        System.out.println("hello world");
+//        System.out.print("hello");
+//        System.out.println("world");
 
 //        예제2-2
-        System.out.print("3+5=");
-        System.out.println(3+5);
+//        System.out.print("3+5=");
+//        System.out.println(3+5);
+
+        //계산값 출력
+        //예제2-3
+        System.out.println("5+3");
+        System.out.println("5-3");
+        System.out.println("5*3");
+        System.out.println("5/3");
+
     }
 
 }
