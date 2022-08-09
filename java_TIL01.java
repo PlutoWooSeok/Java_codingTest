5265cb8b818b2f9f7bc882fb1854c2255d021f25
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-08-09 15:26:43 +0900

    variable type

--- java_TIL01.java
+++ java_TIL01.java
@@ -18,31 +18,31 @@ public class java_TIL01 {
 //        System.out.println("5/3");
         //변수의 선언과 저장
         //예제 2-4
-        int x=5;
-        System.out.println(x);
-
-        x=10;
-        System.out.println(x);
-
-        //예제 2-5
-        int x1=10;
-        int y=5;
-
-        System.out.println(x+y);
-        System.out.println(x-y);
-        System.out.println(x*y);
-        System.out.println(x/y);
-
-        //변수의 타입
-//        int x=100;
-//        double pi = 3.14;
-//        char ch='a';
-//        String str ="abc";
-//
+//        int x=5;
 //        System.out.println(x);
-//        System.out.println(pi);
-//        System.out.println(ch);
-//        System.out.println(str);
+//
+//        x=10;
+//        System.out.println(x);
+//
+//        예제 2-5
+//        int x1=10;
+//        int y=5;
+//
+//        System.out.println(x+y);
+//        System.out.println(x-y);
+//        System.out.println(x*y);
+//        System.out.println(x/y);
+//
+//        변수의 타입
+        int x=100;
+        double pi = 3.14;
+        char ch='a';
+        String str ="abc";
+
+        System.out.println(x);
+        System.out.println(pi);
+        System.out.println(ch);
+        System.out.println(str);
 
 
     }
