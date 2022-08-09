e9362ac95cb3caee6370ee9008d9f9c72c0d3299
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-08-09 15:19:19 +0900

    print text

---
+++ java_TIL01.java
@@ -0,0 +1,14 @@
+public class java_TIL01 {
+    public static void main(String[] args){
+        //화면에 글자 출력하기
+        // 예제 2-1
+        System.out.println("hello world");
+        System.out.print("hello");
+        System.out.println("world");
+
+//        예제2-2
+        System.out.print("3+5=");
+        System.out.println(3+5);
+    }
+
+}
