e53935d7ccef20c79ec5837a1fab6e9d167b83a1
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-08-09 16:51:37 +0900

    Java-TIL- done

--- java_TIL01.java
+++ java_TIL01.java
@@ -151,13 +151,13 @@ public class java_TIL01 {
         //'3' + ""
 
         //예제2-12
-        String str = "3";
-
-        System.out.println(str.charAt(0) - '0');
-        System.out.println('3' - '0' +1);
-        System.out.println(Integer.parseInt("3")+1);
-        System.out.println("3"+1);
-        System.out.println((char) (3+ '0'));
+//        String str = "3";
+//
+//        System.out.println(str.charAt(0) - '0');
+//        System.out.println('3' - '0' +1);
+//        System.out.println(Integer.parseInt("3")+1);
+//        System.out.println("3"+1);
+//        System.out.println((char) (3+ '0'));
     }
 
 }
