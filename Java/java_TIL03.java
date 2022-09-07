7408505426e394cf5cc3e454ed39c5f205c9756d
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-09-07 21:24:37 +0900

    중첩if문

--- Java/java_TIL03.java
+++ Java/java_TIL03.java
@@ -27,7 +27,44 @@ public class java_TIL03 {
 //
 //    }
 //
+    public static void main(String[] args){
+        int score = 0;
+        char grade =' ';
+        char opt = '0';
 
+        System.out.print("점수를 입력하세요>");
+
+        Scanner scanner = new Scanner((System.in)); // 화면을 통해 입력받은 점수를 score에 저장
+        score = scanner.nextInt();
+
+        System.out.printf("당신의 점수는 %d입니다.%n", score);
+        if( score >= 90){   // score 가 90점 보다 같거나 크면 A학점(grade)
+            grade = 'A';
+            if(score >= 98){    // 90점 이상 중에서도 98점 이상은 +
+                opt ='+';
+            } else if (score < 94) {    //90점 이상 94점 미만은 -
+                opt = '-';
+            }
+        } else if (score >=80) {    // score 가 80점 보다 같거나 크면 A학점(grade)
+            grade = 'B';
+            if(score >= 88){    // 80점 이상 중에서도 88점 이상은 +
+                opt ='+';
+            } else if (score < 84) {     //80점 이상 84점 미만은 -
+                opt = '-';
+            }
+        } else if (score >=70) {    // score 가 70점 보다 같거나 크면 A학점(grade)
+            grade = 'C';
+            if(score >= 78){   // 70점 이상 중에서도 78점 이상은 +
+                opt ='+';
+            } else if (score < 74) {    //70점 이상 74점 미만은 -
+                opt = '-';
+            }
+        }else {
+            grade = 'D';    //나머지는 D학점(grade)
+        }
+        System.out.printf("당신의 학점은 %c%c 입니다.%n", grade,opt);
+
+    }
 
 }
 
