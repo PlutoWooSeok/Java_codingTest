4d9bef8f0ccf02366ec3b749596d1360308d44a7
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-09-07 21:09:27 +0900

    if문

---
+++ Java/java_TIL03.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class java_TIL03 {
+    //조건문과 반복문
+    //조건문 : 조건을 만족할 때만 {}를 수행(0~1번)
+    //반복문 : 조건을 만족하는 동안 {}를 수행(0~n번)
+
+    //예제4-4
+//    public static void main(String[] args){
+//        int score = 0;
+//        char grade =' ';
+//
+//        System.out.print("점수를 입력하세요>");
+//        Scanner scanner = new Scanner((System.in));
+//        score = scanner.nextInt();
+//
+//        if( score >= 90){
+//            grade = 'A';
+//        } else if (score >=80) {
+//            grade = 'B';
+//        } else if (score >=70) {
+//            grade = 'C';
+//        }else {
+//            grade = 'D';
+//        }
+//        System.out.println("당신의 학점은 "+grade + "입니다.");
+//
+//    }
+//
+
+
+}
+
