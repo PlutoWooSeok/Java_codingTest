c0a283fc43602649a728fa9ef10f8e131c186d46
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-08 15:25:28 +0900

    시간복잡도 예제코드3

--- Java/시간복잡도_판별원리3.java
+++ Java/시간복잡도_판별원리3.java
@@ -1,9 +1,12 @@
-public class 시간복잡도_판별원리1 {
+public class 시간복잡도_판별원리3 {
     public static void main(String[] args) {
         int N =100000;
         int cnt=0;
         for (int i = 0; i < N; i++){
-            System.out.println("연산 횟수 "+cnt++);
+            for(int j = 0; j < N ; j++ ){
+                System.out.println("연산 횟수 "+cnt++);
+            }
+
         }
     }
 }
