f375ee5dc618875a2e07d8487df62531c08bf493
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-11 20:03:53 +0900

    연습문제017

---
+++ Java/P1427_내림차순정렬.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+//슈도 코드
+/*
+str(정렬할 수)
+A(자릿수별로 구분해 저장한 배열)
+for(str의 길이만큼 반복하기){
+    A 배열 저장 -> str.substring 사용하기
+}
+for(i: 0 ~ str의 길이만큼 반복하기){
+    for(j :i+1 ~ str의 길이만큼 반복하기){
+        현재 범위에서 Max값 찾기
+    }
+    현재 i의 값과 Max값 중 Max값이 더 크면 swap 수행하기
+}
+A 배열 출력하기
+ */
+public class P1427_내림차순정렬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int N = str.length();
+        int[] A = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            int Max = i;
+            for (int j = 0; j < str.length(); j++) {
+                if (A[j] > A[Max]) {
+                    Max = j;
+                }
+                if (A[i] < A[Max]) {
+                    int temp = A[i];
+                    A[i] = A[Max];
+                    A[Max] = temp;
+                }
+            }
+            for (int k = 0; i < str.length(); i++) {
+                System.out.println(A[k]);
+            }
+        }
+    }
+}
