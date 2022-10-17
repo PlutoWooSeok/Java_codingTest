e52012df3e145f11f0b717f72adbcd4421d418c3
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-18 01:14:15 +0900

    연습문제050 : 수정

---
+++ Java/그래프_P1043_거짓말쟁이.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class 그래프_P1043_거짓말쟁이 {
+}
--- Java/그래프_P1976_여행계획짜기.java
+++ Java/그래프_P1976_여행계획짜기.java
@@ -6,25 +6,24 @@ public class 그래프_P1976_여행계획짜기 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();       //도시개수
         int M = sc.nextInt();       //여행경로 데이터
-        int connect[][] = new int[N][N];    //도시간의 이동 가능 여부판단 배열
+        int connect[][] = new int[N+1][N+1];    //도시간의 이동 가능 여부판단 배열
         for(int i = 1; i<=N; i++){
-            for(int j =i; j<=N; j++){
+            for(int j =1; j<=N; j++){
                 connect[i][j] = sc.nextInt();           //연결된 곳은 1, 연결이 되지 않은 부분은 0
             }
         }
-        int [] route = new int[M];                  //여행 경로
+        int [] route = new int[M+1];                  //여행 경로
         for( int i =1; i<=M;i++){
             route[i] = sc.nextInt();            //경로 선택 저장
         }
-        parent = new int[N];                    //대표노드 배열 선언
+        parent = new int[N+1];                    //대표노드 배열 선언
         for(int i = 1 ; i<=N; i++){
             parent[i] =i;                       //대표 노드를 본인의 인덱스 번호로 초기화
         }
         for(int i =1; i<=N;i++){
-            for( int j =1; j<=N; i++){
-                if( connect[i][j] == 1){        //연결이 가능하다면
+            for( int j =1; j<=N; j++){
+                if( connect[i][j] == 1)       //연결이 가능하다면
                     union(i,j);                 //두 노드를 하나로
-                }
             }
         }
         //여행 계획 도시들이 1개의 대표 도시로 연결돼 있는지 확인하기
