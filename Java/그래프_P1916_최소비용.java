b3de19d1c7c6e8894bc2c3047a3e8473b6c66eed
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-20 15:42:44 +0900

    연습문제057:

--- Java/그래프_P1916_최소비용.java
+++ Java/그래프_P1916_최소비용.java
@@ -30,5 +30,76 @@ Edge{
 }
 
  */
+import java.io.*;
+import java.util.*;
+
 public class 그래프_P1916_최소비용 {
+    static int N,M;                     //도시 개수, 버스 개수
+    static ArrayList<Node>[] list;      //인접 리스트로 그래프 표현하기
+    static int[] dist;                  //최단 거리 배열
+    static boolean[] visit;             //사용 노드인지 확인하는 배열
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st ;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        list = new ArrayList[N+1];
+        dist = new int[N+1];
+        visit = new boolean[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        for( int i =0; i <=N; i++){
+            list[i] = new ArrayList<Node>();
+        }
+        for( int i =0; i< M; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list[start].add(new Node(end, weight));
+        }
+        st = new StringTokenizer(br.readLine());
+        int startIndex = Integer.parseInt(st.nextToken());
+        int endIndex = Integer.parseInt(st.nextToken());
+        bw.write(dijkstra(startIndex, endIndex) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int dijkstra(int start, int end) {
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        pq.offer(new Node(start,0));
+        dist[start]=0;
+        while (!pq.isEmpty()){
+            Node nowNode = pq.poll();
+            int now = nowNode.targetNode;
+            if(!visit[now]){
+                for(Node n : list[now]){
+                    if(!visit[n.targetNode] && dist[n.targetNode] > dist[now] + n.value){
+                        dist[n.targetNode] = dist[now] + n.value;
+                        pq.add(new Node(n.targetNode, dist[n.targetNode]));
+                    }
+                }
+            }
+        }
+        return dist[end];
+    }
+
+}
+
+class Node implements Comparable<Node>{
+    int targetNode;
+    int value;
+    Node(int targetNode, int value){
+        this.targetNode = targetNode;
+        this.value = value;
+    }
+
+
+
+    @Override
+    public int compareTo(Node o) {
+        return value -o.value;
+    }
 }
