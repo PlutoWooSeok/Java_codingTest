0f554128c63964cf91ca7d10e88c0bcdc56b9fe3
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-21 19:29:00 +0900

    연습문제062:

---
+++ Java/그래프_P11403_경로찾기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 그래프_P11403_경로찾기 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int N;
+    public static int [][] A;
+    public static void main(String[] args) throws Exception{
+        N = Integer.parseInt(br.readLine());
+        A = new int[N+1][N+1];
+        for( int i = 1; i<=N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for( int j =1; j<=N; j++){
+                int v = Integer.parseInt(st.nextToken());
+                A[i][j] = v;
+            }
+        }
+        for( int k=0; k <N; k++){
+            for( int i = 1; i<=N; i++){
+                for( int j = 1; j<=N; j++){
+                    if (A[i][k] ==1 && A[k][j] ==1)
+                        A[i][j]=1;
+                }
+            }
+        }
+        for( int i = 1; i<=N; i++){
+            for (int j = 1; j<=N; j++){
+                System.out.print(A[i][j]  + " ");
+            }
+            System.out.println();
+        }
+    }
+}
