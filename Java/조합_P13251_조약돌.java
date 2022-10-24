2a60fdfb0dcb29fc661a00fc667123bbad6bf7ee
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-24 10:03:01 +0900

    연습문제080:조합: 주석처리

--- Java/조합_P13251_조약돌.java
+++ Java/조합_P13251_조약돌.java
@@ -42,29 +42,30 @@ import java.util.StringTokenizer;
  */
 public class 조합_P13251_조약돌 {
     public static void main(String[] args) throws IOException {
-        int M,K,T;
-        int D[] = new int[51];
-        double prob[] = new double[51];
-        double ans;
+        int M,K,T;                                              //T : 케이스 테스트 횟수     M : 색상 종류    K: 선택 조약돌 개수
+        int D[] = new int[51];                              //각 색깔별 조약돌 개수 저장하기
+        double prob[] = new double[51];                                 //각 색깔별 확률 저장 배열
+        double ans;                                 //정답 변수
         T =0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());                           //색상 종류 저장
         st = new StringTokenizer(br.readLine());
         for( int i = 0; i<M; i++){
-            D[i] = Integer.parseInt(st.nextToken());
-            T += D[i];
+            D[i] = Integer.parseInt(st.nextToken());                    //D 배열에 각 조약돌 개수 저장하기
+            T += D[i];                                                  // T 변수에 조약돌 개수 더하기
         }
         st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());                                           //K 변수 저장하기
         ans = 0.0;
-        for( int i =0; i<M; i++){
-            if( D[i] >= K){
+        for( int i =0; i<M; i++){                   //선택 조약돌 개수보다 현재 색 조약돌 개수가 적은면 모두 같은 색으로 뽑을 확률 0
+            if( D[i] >= K){                 //현재 색의 조약돌의 개수가 선택해야 할 개수보다 클 경우
                 prob[i] = 1.0;
-                for (int k=0; k<K; k++)
-                    prob[i] *= (double) (D[i] -k)/(T-K);
+                for (int j=0; j<K; j++)
+                    //i 색상을 모두 뽑을 확률 = i 색상을 모두 뽑을 확률 * 현재 색상 개수 - K/ 전체 색상 개수 - K
+                    prob[i] *= (double) (D[i] -K)/(T-K);
             }
-            ans += prob[i];
+            ans += prob[i];                         //정답에 현재 색상을 모두 뽑을 확률 더하기
         }
         System.out.println(ans);
     }
