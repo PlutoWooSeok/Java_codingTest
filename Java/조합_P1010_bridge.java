1ce3b824c64f6dadfb1cf889ad376aa9c7d5f0a8
PlutoWooSeok <plutoxxx1014@gmail.com>
2022-10-24 10:07:52 +0900

    연습문제079

--- Java/조합_P1010_bridge.java
+++ Java/조합_P1010_bridge.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class 조합_P1010_bridge {
+import java.util.Scanner;
+
+public class 조합_P1010_bridge {
+    static int T,N,M;
+    static int[][]D;
+
+    public static void main(String[] args){
+        D = new int[31][31];
+        for( int i =0; i<31;i++){
+            D[i][i] =1;
+            D[i][1] = i;
+            D[i][0] = 1;
+        }
+        for( int i = 2; i<31;i++){
+            for( int j =1; j<i;j++){
+                D[i][j] = D[i-1][j] + D[i-1][j-1];
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+        for (int i = 0; i<T;i++){
+            N = sc.nextInt();
+            M = sc.nextInt();
+            System.out.println(D[M][N]);
+        }
+    }
 }
